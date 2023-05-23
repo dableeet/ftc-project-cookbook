ceab0646ef9a411e79aa50358e8e0ed0b1ab7f3a
TereshinaElena <elena-crane@yandex.ru>
2023-05-23 17:27:33 +0300

    add navbar

--- src/views/NavBar.jsx
+++ src/views/NavBar.jsx
@@ -3,25 +3,29 @@ const React = require("react");
 module.exports = function NavBar({ user }) {
   return (
     <nav className="navBar">
+      <a href="/">
+        <img src="" alt="Logo" />
+      </a>
       {user ? (
-        <>
-          <span>hello</span>
-          <a href="/">
-            home
-          </a>
-          <a href="/login/logout">
-            logout
-          </a>
-        </>
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Menu
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/">Favourite</a></li>
+            <li><a class="dropdown-item" href="/login/logout">Logout</a></li>
+          </ul>
+        </div>
       ) : (
-        <>
-          <a href="/login">
-            login
-          </a>
-          <a href="/login/registration">
-            registration
-          </a>
-        </>
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Menu
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/login">Login</a></li>
+            <li><a class="dropdown-item" href="/login/registration">Registration</a></li>
+          </ul>
+        </div>
       )}
     </nav>
   );
---
+++ src/views/ShowRecipe.jsx
@@ -0,0 +1,13 @@
+const React = require("react");
+const Layout = require("./Layout");
+
+module.exports = function ShowRecipe() {
+  return (
+    <Layout>
+      <div>
+
+      </div>
+    </Layout>
+  );
+};
+
