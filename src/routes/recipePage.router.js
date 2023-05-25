05012df65b4bb0dd016dc614c2c729f56f1e27b1
TereshinaElena <elena-crane@yandex.ru>
2023-05-25 12:03:43 +0300

    favourite btn in showRecipe

--- src/routes/recipePage.router.js
+++ src/routes/recipePage.router.js
@@ -7,8 +7,17 @@ router.get('/:id', async (req, res) => {
   const { id } = req.params;
   const recipe = await Recipes.findOne({ where: { id } });
   const clearRecipe = recipe.get();
-
-  res.render(ShowRecipe, { clearRecipe });
+  if (clearRecipe.instructions.includes('<ol>')) {
+    const a = clearRecipe.instructions.replaceAll('<ol>', '');
+    const b = a.replaceAll('<li>', '');
+    const c = b.replaceAll('</li>', ' ');
+    const d = c.replaceAll('</ol>', '');
+    clearRecipe.instructions = d;
+  }
+  const favourites = await Users.findOne({ where: { id: req.session.user.id } });
+  const clearFavourites = favourites.get();
+  const isFavourite = clearFavourites.favourite?.includes(Number(id));
+  res.render(ShowRecipe, { clearRecipe, isFavourite });
 });
 
 module.exports = router;
--- src/views/ShowRecipe.jsx
+++ src/views/ShowRecipe.jsx
@@ -1,49 +1,51 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-module.exports = function ShowRecipe({ user, clearRecipe }) {
-  console.log(clearRecipe);
+module.exports = function ShowRecipe({ user, clearRecipe, isFavourite }) {
   return (
     <Layout user={user}>
+      <script defer src="/js/favouriteRecipes.js" />
       <div className="showRecipeWrapper">
-
         <div className="showRecipeContainer">
 
           <div className="showRecipeHeader">
 
             <div className="showRecipeHeaderLeftSide">
-              <div className="showRecipeFoto">
-                <img src={clearRecipe.image} alt="foto dish" />
-              </div>
-              <div className="showRecipeFavourite">
-                <button type="button" className="text-green-700 hover:text-white border border-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-1 dark:border-green-500 dark:text-green-500 dark:hover:text-white dark:hover:bg-green-600 dark:focus:ring-green-800">Добавить в избранное</button>
-              </div>
+              <div className="showRecipeFoto mx-2" style={{ backgroundImage: `url(${clearRecipe.image})` }}></div>
+              {
+                isFavourite ? (
+                  <div className="showRecipeFavourite">
+                    <button type="button" className="text-white hover:text-secondary-500 border bg-green-400 hover:bg-white hover:border-secondary-500 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-1">Удалить из избранного</button>
+                  </div>
+                ) : (
+                  <div className="showRecipeFavourite">
+                    <button type="button" className="text-secondary-500 hover:text-white border border-secondary-500 hover:bg-secondary-500 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center m-1">Добавить в избранное</button>
+                  </div>
+                )
+              }
             </div>
 
-            <div className="showRecipeHeaderRightSide">
-              <div className="showRecipeTitle m-1">
-                <h1>{clearRecipe.title}</h1>
+            <div className="showRecipeHeaderRightSide mx-3 mt-3">
+              <div className="showRecipeTitle">
+                <h1 className="text-center text-xl">{clearRecipe.title}</h1>
               </div>
-              <div className="showRecipeIngredients m-1">
-                <ul>
-                  Ingredients:
+              <div className="showRecipeIngredients ml-5 mt-2">
+                <ul className="list-disc">
+                  <strong>Ingredients:</strong>
                   {clearRecipe.extendedIngredients.map((el) => (
-                    <li>{el.original}</li>))}
+                    <li key={el.id}>{el.original}</li>))}
                 </ul>
               </div>
             </div>
 
           </div>
 
-          <div className="showRecipeBody">
+          <div className="showRecipeBody p-3">
             {clearRecipe.instructions}
-            {clearRecipe.instructions}
-
           </div>
-
         </div>
 
       </div>
-    </Layout>
+    </Layout >
   );
 };
